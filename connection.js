9d04598adf3a4e36a37b6916a3644628fd9d6a12
ayman-fer <97614053+ayman-fer@users.noreply.github.com>
2022-02-28 23:16:28 +0100

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 const { Client } = require("pg");
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.postgres://wcqbfinzjbfzoa:f07c522e07ca77c2046b568b2b1791a62bea2d38e361e189043ae642a44ae849@ec2-54-156-110-139.compute-1.amazonaws.com:5432/dfuh9ki3jm5095,
   ssl: {
     rejectUnauthorized: false,
   },
